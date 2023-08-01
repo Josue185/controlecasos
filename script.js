4615a42b7a42433baeeeb6ac88730a6595fe9891
Josué Dos Santos Domingues <92592495+Josue185@users.noreply.github.com>
2023-08-01 12:43:44 -0300

    Update script.js

--- script.js
+++ script.js
@@ -12,24 +12,12 @@ const handleSubmit = (event) => {
     event.preventDefault();
     adicionaLoading();
 
-    const Agente = document.querySelector("#Agente").value;
     const LDAP = document.querySelector("#LDAP").value;
-    const cognizantID = document.querySelector("#cogId").value;
-    const tL = document.querySelector("#teamLeader").value;
-    const dataOfAbstance = document.querySelector("#dataOfAbsance").value;
-    const dataOfReturn = document.querySelector("#dateOfReturn").value;
-    const totalDay = document.querySelector("#totalDay").value;
-    const bussinesDay = document.querySelector("#bussinesDay").value;
-    const reason = document.querySelector("#reason").value;
-    const type = document.querySelector("#type").value;
-    const documentation = document.querySelector("#documentation").value;
-    const month = document.querySelector("#month").value;
-    const year = document.querySelector("#year").value;
-    const comments = document.querySelector("#comments").value;
-    const screenshoot = document.querySelector("#screenshoot").value;
-
-
- 
+    const dataAtendimento = document.querySelector("#dataAtendimento").value;
+    const numeroCaso = document.querySelector("#numeroCaso").value;
+    const statusAtendimento = document.querySelector("#statusAtual").value;
+    const novoStatus = document.querySelector("#statusAtualizados").value;
+    const print = document.querySelector("#screenshoot").value;
     
     
     fetch('https://api.sheetmonkey.io/form/jzi1PQfx9zyXR7TjiMHLzN', {
@@ -38,7 +26,7 @@ const handleSubmit = (event) => {
             'Accept': 'application/json',
             'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ Agente, LDAP, cognizantID, tL, dataOfAbstance, dataOfReturn, totalDay, bussinesDay, month, year, comments, screenshoot})
+        body: JSON.stringify({LDAP, dataAtendimento, numeroCaso, statusAtendimento, novoStatus, print})
     })
         .then(() => {
             removeLoading();
