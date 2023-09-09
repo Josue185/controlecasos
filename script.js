91f62fa3eb6c276de8f5eaa1595ef87e01cd7dee
Josué Dos Santos Domingues <92592495+Josue185@users.noreply.github.com>
2023-09-09 15:10:00 -0300

    Update script.js

--- script.js
+++ script.js
@@ -44,3 +44,52 @@ const handleSubmit = (event) => {
 };
 
 document.querySelector('#formControle').addEventListener('submit', handleSubmit);
+
+//Firebase
+
+
+// Importe as funções necessárias do SDK do Firebase
+import { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/10.3.1/firebase-firestore.js";
+
+// Obtenha uma referência para o Firestore
+const db = getFirestore();
+
+// Obtenha referências para os elementos do formulário
+const form = document.getElementById("formControle");
+const LDAP = document.getElementById("LDAP");
+const dataAtendimento = document.getElementById("dataAtendimento");
+const numeroCaso = document.getElementById("numeroCaso");
+const statusAtual = document.getElementById("statusAtual");
+const statusAtualizados = document.getElementById("statusAtualizados");
+const tarefas = document.getElementById("tarefas");
+const screenshot = document.getElementById("screenshot");
+const horacaso = document.getElementById("horacaso");
+const time = document.getElementById("time");
+
+// Adicione um ouvinte de evento para o envio do formulário
+form.addEventListener("submit", async (e) => {
+  e.preventDefault(); // Impede o envio padrão do formulário
+
+  // Crie um objeto com os dados do formulário
+  const formData = {
+    LDAP: LDAP.value,
+    dataAtendimento: dataAtendimento.value,
+    numeroCaso: numeroCaso.value,
+    statusAtual: statusAtual.value,
+    statusAtualizado: statusAtualizados.value,
+    tarefas: tarefas.value,
+    screenshot: screenshot.value,
+    horacaso: horacaso.value,
+    time: parseInt(time.value), // Certifique-se de que o tempo seja interpretado como um número
+  };
+
+  try {
+    // Adicione os dados do formulário ao Firestore (ou Realtime Database, se estiver usando)
+    await addDoc(collection(db, "casos"), formData);
+    alert("Dados do formulário enviados com sucesso!");
+    form.reset(); // Limpa o formulário após o envio
+  } catch (error) {
+    console.error("Erro ao enviar dados:", error);
+  }
+});
+
